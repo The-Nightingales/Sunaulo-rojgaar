3927a86e60a26c98ea4b14a3a2bc9976a7203fdb
Zyotiee111 <zyotieebista12@gmail.com>
2020-01-24 17:34:02 +0545

    Add files via upload

---
+++ employeelogin.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+     <title>Login Form</title>
+     <link rel="stylesheet" type="text/css" href="companylogin.css">
+   </head>
+  <body>
+<div class="reg">
+    <h1>Employee Login</h1>
+
+<form action="action_page.php" method="post">
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+  
+ 
+    
+  </div>
+</form>
+</div>
+  <br>
+  <br>
+  <a href="companyregister.php"><b>New User? Register Here !</b></a>
+</form>
